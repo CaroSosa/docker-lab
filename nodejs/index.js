be86c5a106fafba82877388a9632b2807e884ee1
CaroSosa <anacarolinasosaa@gmail.com>
2022-02-10 11:43:49 -0300

    lab entero

---
+++ nodejs/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path')
+
+// Constants
+const PORT = 3000;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, "indexnode.html")); 
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
